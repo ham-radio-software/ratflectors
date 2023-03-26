d5741b8d8df62053309ca8dddd43dc2a3ff7df8b
John E. Malmberg <wb8tyw@gmail.com>
2023-03-26 15:09:31 -0500

    Initial Ratflrector List

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# ratflectors
+# Public Ratflectors
+
 Known Public Ratflectors for D-Rats use
+
+This is a YAML file that is planned to be used to assist in managing
+maintaining Internet Ratflector lists for D-Rats.
+
+This file is maintained in [YAML](https://yaml.org/spec/) format
+
+Each entry has the following members:
+
+* Name, a short name, about 8 characters may be normally visible.
+* Description, More detailed description.
+* Hostname, Fully qualified host name.
+* Port, port number.
+* Active, true for active at last update.
+
+An optional password member is if a password is needed.  The password
+should not be stored in this file.
+
+Advanced maintainers can copy the tests/pre-commit script to .git/hooks which if the proper tools are installed, will test the format of the files in a
+git commit action.  This requires shellcheck, yamllint, and codespell to be installed.
+
+```yaml
+---
+ratflectors:
+  - name: short_name
+    description: |
+      Long description of the Ratflector that can take
+      multiple lines, including contact information if known.
+    hostname: ratflector.example.com
+    port: 9000
+    active: true
+    password: false
+```
---
+++ tests/pre-commit_d/shellcheck_check.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -ue
+
+# Default list of files to lint
+: "${CHANGED_FILES:=}"
+if [ -z "$CHANGED_FILES" ]; then
+  # Nothing to do
+  exit 0
+fi
+
+# Default output
+: "${SHELLCHECK_OUT:=shellcheck.log}"
+# Default directory to start check
+: "${BASE_DIR:=.}"
+
+# Follow external references if shellcheck supports it.
+external=
+if (shellcheck --help | grep "\-\-external") &> /dev/null; then
+  external=--external
+fi
+
+# Now search for all shell script files
+rc=0
+pushd "${BASE_DIR}" > /dev/null || exit 1
+
+  rm -f "${SHELLCHECK_OUT}"
+
+  # Template to allow other tools to parse the output
+  tmpl="--format=gcc"
+
+  if ! shellcheck="$(command -v shellcheck)"; then
+    echo "shellcheck not found"
+    exit 0
+  fi
+
+  for script_file in ${CHANGED_FILES}; do
+
+    IFS=" " read -r -a shellcheck_cmd <<< \
+        "${shellcheck} ${external} ${script_file}"
+    if file "$script_file" | grep -q -e 'shell script'; then
+      if ! "${shellcheck_cmd[@]}" "${tmpl}" >> "${SHELLCHECK_OUT}" 2>&1; then
+        (( rc=rc+PIPESTATUS[0] ))
+      fi
+    fi
+  done
+  if [ "$rc" -ne 0 ] && [ -e "${SHELLCHECK_OUT}" ]; then
+    if ! grep ':' "${SHELLCHECK_OUT}"; then
+      cat "${SHELLCHECK_OUT}"
+    fi
+  fi
+popd > /dev/null || exit 1
+exit "${rc}"
